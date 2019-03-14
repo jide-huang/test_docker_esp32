81e252c4d2bcf4fbfd5778388b28c78f17a7045f
jide-huang <jd@gooee.com>
2019-03-14 15:17:20 +0800

    A

---
+++ hello_world/build/bootloader/spi_flash/spi_flash_rom_patch.d
@@ -0,0 +1,33 @@
+spi_flash_rom_patch.o: \
+ C:/msys32/home/jdhua/esp/esp-idf/components/spi_flash/spi_flash_rom_patch.c \
+ C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/rom/ets_sys.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/esp_assert.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/rom/gpio.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/esp_attr.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/gpio_reg.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/rom/spi_flash.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/spi_reg.h \
+ d:\exgithub\under_test\test_docker_esp32\hello_world\build\include\sdkconfig.h
+
+C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/rom/ets_sys.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/esp_assert.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/rom/gpio.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/esp_attr.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/gpio_reg.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/esp32/include/rom/spi_flash.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/soc/esp32/include/soc/spi_reg.h:
+
+d:\exgithub\under_test\test_docker_esp32\hello_world\build\include\sdkconfig.h:
+/home/jdhua/esp/esp-idf/components/spi_flash/./spi_flash_rom_patch.c:
