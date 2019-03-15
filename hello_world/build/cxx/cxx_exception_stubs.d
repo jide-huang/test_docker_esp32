79cced00c2c190c69187532075582293dbf7d757
jide-huang <jd@gooee.com>
2019-03-15 13:54:26 +0800

    AA

---
+++ hello_world/build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ C:/msys32/home/jdhua/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
+ D:/ExGithub/under_test/test_docker_esp32/hello_world/build/include/sdkconfig.h
+
+D:/ExGithub/under_test/test_docker_esp32/hello_world/build/include/sdkconfig.h:
+/home/jdhua/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
---
+++ hello_world/build/mbedtls/mbedtls/library/memory_buffer_alloc.d
@@ -0,0 +1,18 @@
+mbedtls/library/memory_buffer_alloc.o: \
+ C:/msys32/home/jdhua/esp/esp-idf/components/mbedtls/mbedtls/library/memory_buffer_alloc.c \
+ C:/msys32/home/jdhua/esp/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h \
+ D:/ExGithub/under_test/test_docker_esp32/hello_world/build/include/sdkconfig.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/check_config.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/newlib/include/limits.h \
+ C:/msys32/home/jdhua/esp/esp-idf/components/newlib/include/newlib.h
+
+C:/msys32/home/jdhua/esp/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h:
+
+D:/ExGithub/under_test/test_docker_esp32/hello_world/build/include/sdkconfig.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/check_config.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/newlib/include/limits.h:
+
+C:/msys32/home/jdhua/esp/esp-idf/components/newlib/include/newlib.h:
+/home/jdhua/esp/esp-idf/components/mbedtls/mbedtls/library/memory_buffer_alloc.c:
